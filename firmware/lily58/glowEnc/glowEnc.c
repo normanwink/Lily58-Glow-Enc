4db69d40515172a4e2e5a5e7129a290f0e9536bb
orvis evans <b.orvis@gmail.com>
2020-09-08 00:00:09 -0700

    Better lighting setup on left side

--- firmware/lily58/glowEnc/glowEnc.c
+++ firmware/lily58/glowEnc/glowEnc.c
@@ -13,17 +13,13 @@ void led_set_kb(uint8_t usb_led) {
 led_config_t g_led_config = { {
   // Key Matrix to LED Index
   // Left Side
-  { 29, 28, 27, 26, 25, 24 },
-  { 18, 19, 20, 21, 22, 23 },
-  { 17, 16, 15, 14, 13, 12 },
-  { 6, 7, 8, 9, 10, 11 },
-  { 5, 4, 3, 2, 1, 0 },
-  // Right Side
-  { NO_LED, NO_LED, NO_LED, 38    , 37    , 36     },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED }
+  { 24, 25, 26, 27,     28,     29     },
+  { 23, 22, 21, 20,     19,     18     },
+  { 12, 13, 14, 15,     16,     17     },
+  { 11, 10, 9,  8,      7,      6      },
+  { 0 , 1,  2,  3,      4,      5      },
+  { 30, 31, 32, NO_LED, NO_LED, NO_LED },
+  { 35, 34, 33, NO_LED, NO_LED, NO_LED }
 }, {
   // LED Index to Physical Position`
   // Left Side (Bottom to top, left to right, every other row is backwards)
@@ -32,12 +28,16 @@ led_config_t g_led_config = { {
   { 0,   32 }, { 20,  32 }, { 40,  32 }, { 60,  32 }, { 80,  32 }, { 100, 32 }, 
   { 100, 3  }, { 80,  3  }, { 60,  3  }, { 40,  3  }, { 20,  3  }, { 0,   3  },
   { 0,   16 }, { 20,  16 }, { 40,  16 }, { 60,  16 }, { 80,  16 }, { 100, 16 }, 
+  // Left Rear
+  { 100, 3  }, { 60,  3  }, { 20,  3  }, { 20,  48 }, { 60,  64 }, { 100, 64 }
   // Right Side
-  { 140, 3 }, { 160, 3 }, { 180, 3 }, { 0,   0 }, { 0,   0 }, { 0,   0 },
-  { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 },
-  { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 },
-  { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 },
-  { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }
+  // { 140, 3 }, { 160, 3 }, { 180, 3 }, { 0,   0 }, { 0,   0 }, { 0,   0 },
+  // { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 },
+  // { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 },
+  // { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 },
+  // { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 }, { 0,   0 },
+  // // Right Rear
+  // { 240, 3 }, { 210, 3 }, { 180, 3 }, {180,  48}, {210,  48}, {240,  48},
 }, {
   // LED Index to Flag
   // Left Side (bottom to top, left to right, every other row is backwards)
@@ -46,12 +46,7 @@ led_config_t g_led_config = { {
    1, 4, 4, 4, 4, 4,
    4, 4, 4, 4, 4, 1,
    4, 4, 4, 4, 4, 4,
-  // Right Side (bottom to top, left to right)
-   4, 4, 4, 4, 4, 4,
-   4, 4, 4, 4, 4, 4,
-   4, 4, 4, 4, 4, 4,
-   4, 4, 4, 4, 4, 4,
-   4, 4, 4, 4, 4, 4,
+   2, 2, 2, 2, 2, 2,
 } };
 
 #endif // RGB_MATRIX_ENABLE
